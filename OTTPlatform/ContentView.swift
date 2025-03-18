a861f0505a5a02bba9d19c9d000c8040ca1f6031
Qonobalakrishnan <balakrishnan@pofitec.in>
2025-03-18 08:33:38 +0530

    Initial Commit

---
+++ OTTPlatform/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OTTPlatform
+//
+//  Created by Apple8 on 18/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OTTPlatform/OTTPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTTPlatformApp.swift
+//  OTTPlatform
+//
+//  Created by Apple8 on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct OTTPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
